1d58badd54570c92934c4d4ff72997432f942164
igor <gob1in@Mail.ru>
2022-02-22 19:10:47 +0300

    Domaha

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    String nameOfTheMan = "Ivan";
+    int height = 186;
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello Вася") + args[0];
+    }
+    String
+}
