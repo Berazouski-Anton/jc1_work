4230b34dfa145ba4dcdebd2541eb759b31bd23d7
igor <gob1in@Mail.ru>
2022-02-24 21:27:36 +0300

    dobavka

--- my/car/Main.java
+++ my/car/Main.java
@@ -2,6 +2,14 @@ package my.car;
 
 public class Main {
     public static void main(String[] args) {
+Man myMan2;
+Man myMan = new Man();
+System.out.println(myMan.getAge());
 
+myMan2 = myMan;
+System.out.println(myMan2.getAge());
+myMan2.age = 25;
+System.out.println(myMan2.getAge());
+System.out.println(myMan.getAge());
     }
 }
