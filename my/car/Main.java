18cc0519b9cf27012d6566f8deb115ba20efa586
igor <gob1in@Mail.ru>
2022-02-24 20:06:27 +0300

    peremennie

---
+++ my/car/Main.java
@@ -0,0 +1,7 @@
+package my.car;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- my/car/cars.java
+++ my/car/cars.java
@@ -1,7 +1,20 @@
 package my.car;
 
+import javax.swing.*;
+
 public class cars {
     public class Man
     int age = 18;
-    int getAge(){}
-}
+    Spring firsName = "Vasia";
+    char nameLatter 'V';
+    int binAge = 0b010101;
+    int octAge = 025;
+    int hexAge = 0xFF;
+    long cell = 1_000_000L;//true
+    float weight = 1.11F;
+    double height = 0.34d;
+    boolean isSingle = false;
+    short s1 = 123;
+    byte b1= 123;
+
+
